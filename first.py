f17febfc7035726046a5ec6adc0c58f40489d74f
gmlincoln <maula03@gmail.com>
2024-07-12 09:13:09 +0600

    initialize-variable-concat-string

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+
+myName = 'Lincoln'
+age = 100
+currentCourse = 'WADP'
+
+def myDetails():
+    print("Name: "+myName)
+    print("Age: ",age)
+    print("Enrolled Courser: "+currentCourse)
+
+myDetails()
